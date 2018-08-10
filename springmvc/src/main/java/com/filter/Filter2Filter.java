efae6561983cffa5b9e0dd9690d4f3367247218d
TRAN VAN PHU <TRAN VAN PHU@DESKTOP-JF7FRMU.net.fpt>
2018-08-11 00:04:02 +0700

    Added filters

---
+++ springmvc/src/main/java/com/filter/Filter2Filter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author TRAN VAN PHU
+ */
+public class Filter2Filter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filter 2");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}
